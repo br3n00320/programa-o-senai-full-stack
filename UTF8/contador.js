869ced292b66fb5947df3f445b16db177c1126b9
br3n00320 <62916391+br3n00320@users.noreply.github.com>
2022-07-26 15:20:05 -0300

    26/07

---
+++ UTF8/contador.js
@@ -0,0 +1,20 @@
+ 
+let numeroDeAlunos = 10;
+for (let contador = 0; contador <= numeroDeAlunos; contador++)
+{
+
+// console.log (contador)
+
+if (contador == 0 ){
+
+console.log(`o numero ${contador} é zero`)
+}else if (contador % 2 == 0 ){
+
+    console.log (`numero ${contador} é par`)
+
+}else{
+
+    console.log (`numero ${contador} é impar`)
+}
+
+}
