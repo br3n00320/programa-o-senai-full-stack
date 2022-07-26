5d1f1292167a28e9cbabc85db7fafba17d49b1d1
br3n00320 <62916391+br3n00320@users.noreply.github.com>
2022-07-26 17:25:23 -0300

    Create UTF8.js

---
+++ UTF8/UTF8.js
@@ -0,0 +1,34 @@
+
+let dat = 20; mes = 03; day = 03; mesDay = 03
+
+console.log (`*** ${dat} será o dia do evento do mês ${mes} *** \n  ***O dia atual da escrição é  ${day}/${mesDay}***  `)
+
+if (dat > day && mes >= mesDay){
+    
+    console.log ("************")
+    console.log ("data valida")
+
+let idade = 19; 
+
+        if (idade >= 18){
+            console.log ("************ \nidade valida, continue com o cadastro. \n ************")
+
+            console.log ('verificando de há vagas... \n ************ ')
+
+            var vagas = ["breno", "joao", "carlos", "vitor", "beatriz"]
+                        if (vagas.length < 3 ){
+                            console.log ("parabens cadastro realizado com sucesso!!!")
+                        }else{
+                            console.log ("cadastro não realizado com sucesso!! As vagas já foram prenchidas ")
+                        }
+
+            }else{
+                console.log("idade invalida, não poderá participar do envento")
+                    }
+
+}else{
+
+    console.log ("data invalida")
+ }
+
+
